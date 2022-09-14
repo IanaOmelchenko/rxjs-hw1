86c2a6067a0d6cfebdc482ef9440acf49cd759b2
IanaOmelchenko <23176526+IanaOmelchenko@users.noreply.github.com>
2022-09-13 22:35:41 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,20 +1,22 @@
 import './style.css';
 
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, of, interval, take, map } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 // Створити Observable, яка буде віддавати 2 синхронні значення "1", "2", а через 2 секунди викидувати помилку. Ваша задача використовуючи існуючі оператори обробити цю помилку всередині pipe, для того, щоб вона не дійшла до subscribe
 
-const observable = new Observable((subscriber) => {
-  subscriber.next(1);
-  subscriber.next(2);
-  setTimeout(() => subscriber.error('Error from Observable'), 2000);
-}).pipe(catchError(catchError((val) => of(`I caught: ${val}`))));
+// const observable = new Observable((subscriber) => {
+//   subscriber.next(1);
+//   subscriber.next(2);
+//   setTimeout(() => subscriber.error('Error from Observable'), 2000);
+// }).pipe(catchError(catchError((val) => of(`I caught: ${val}`))));
 
-observable.subscribe({
-  next: (val) => console.log('THIS IS VALUE', val),
-  error: (err) => console.log('THIS IS ERROR', err),
-});
+// observable.subscribe({
+//   next: (val) => console.log('THIS IS VALUE', val),
+//   error: (err) => console.log('THIS IS ERROR', err),
+// });
+
+//********************************************************************* */
 
 // Створити аналог fromEvent оператора( який під капотом використовує addEventListener).
 // Не забувайте про витоки пам'яті і те, як їх уникати в RxJS(після відписання від цього оператора ми не повинні більше слухати події)
@@ -30,9 +32,17 @@ function newFromEvent(el, actionType) {
   });
 }
 
+//********************************************************************* */
+
 // Використовуючи оператор interval, підписатися на нього і слухати до того моменту, доки значення не буде більше 5(використовуючи оператор в pipe)
 
+// const intervalPipe = interval().pipe(take(5));
+// intervalPipe.subscribe({
+//   next: (val) => console.log('THIS IS VALUE', val),
+//   error: (err) => console.log('THIS IS ERROR', err),
+// });
 
+//********************************************************************* */
 
 // Перетворіть coldInterval нижче на hotInterval, щоб він став гарячим(віддавав одні і ті ж значення різним підписникам)
 // Приклад:
@@ -69,16 +79,39 @@ function newFromEvent(el, actionType) {
 //   3000
 // );
 
+//********************************************************************* */
+
 // Обробити відповідь запиту, в pipe спочатку витягнути об'єкт response(це масив), відфільтруєте масив так, щоб залишилися тільки пости з id менше 5.
 // Hint: так як response - це буде масив постів, ви не можете просто фідфільтрувати його через filter(він приймає кожен елемент масиву, а не цілий масив). Для рішення цієї задачі вам потрібні оператори mergeMap або concatMap, в яких ви зробите з(перекладіть англійською) масиву потік окремих елементів масиву([1, 2, 3] => 1, 2, 3), відфільтруєте їх,а потім зберете назад в масив за допомогою оператора. В subscribe ми отримаємо масив з 4 об'єктів id яких менше 5
 
-ajax('https://jsonplaceholder.typicode.com/posts');
+const posts$ = ajax.getJSON('https://jsonplaceholder.typicode.com/posts').pipe(
+  map((result) => {
+    console.log('*** result ***', result);
+    console.log('*** result ***', typeof result);
+    return result;
+  }),
+  catchError((error) => {
+    console.log('error: ', error);
+    return of(error);
+  })
+);
+
+posts$.subscribe({
+  next: (value) => {
+    console.log('THIS IS A VALUE', typeof value);
+  },
+  error: (err) => console.log(err),
+});
+
+//********************************************************************* */
 
 // Використовуючи Rxjs написати потік, який буде слухати кліки по кнопці і відправляти при натисканню на неї запит на сервер із текстом введеним в пошук. В subscribe ми маємо отримати дані з серверу.
 // Оператори, які можуть знадобитися: fromEvent, switchMap, ajax, map, etc
 
 const search = document.querySelector('input');
 const button = document.querySelector('button');
+
+//********************************************************************* */
 
 // Використовуючи RxJs зробити свою імплементацію Drag&Drop.
 // Деталі: Створіть 3 observable mousedown$, mousemove$, mouseup$. Які будуть слухати події mousedown, mousemove, mouseup відповідно. Ваша задача поєднати їх так, щоб mousemove$ починав працювати тільки коли користувач натикає  на mousedown, і переставали слухати, коли відбувається mouseup. Тобто постійно ви маєте слухати тільки mousedown, а підписуватися на зміну mousemove i mouseup тільки після івенту mousedown
